51437645dae4d31cba4c20c071618110e9749c15
Antonio Jesús <antoniojesuspv99@gmail.com>
2023-01-28 21:53:17 +0100

    Configuración inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
       ),
-      home: MyHomePage(),
     );
   }
 }
