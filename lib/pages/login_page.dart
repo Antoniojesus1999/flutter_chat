932751fd359cbe2828c891f293af9345f96fc6f4
Antonio Jesús <antoniojesuspv99@gmail.com>
2023-01-29 21:49:16 +0100

    loging 50%

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,99 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('login'),
+      backgroundColor: const Color(0xffF2F2F2),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            _Logo(),
+            _Form(),
+            _Labels(),
+            Text(
+              'Términos y condiciones de uso',
+              style: TextStyle(fontWeight: FontWeight.w200),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//Pinta el logo
+class _Logo extends StatelessWidget {
+  const _Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 170,
+        margin: const EdgeInsets.only(top: 50),
+        child: Column(
+          children: const [
+            Image(image: AssetImage('assets/logo.png')),
+            SizedBox(height: 20),
+            Text('Messenger', style: TextStyle(fontSize: 30))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  const _Form({super.key});
+
+  @override
+  State<_Form> createState() => _FormState();
+}
+
+class _FormState extends State<_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          const TextField(),
+          const TextField(),
+          ElevatedButton(
+            onPressed: () {},
+            child: null,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Labels extends StatelessWidget {
+  const _Labels({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: const [
+          Text(
+            '¿No tiense cuenta?',
+            style: TextStyle(
+                color: Colors.black54,
+                fontSize: 15,
+                fontWeight: FontWeight.w300),
+          ),
+          SizedBox(height: 10),
+          Text('Cree una ahora!',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 41, 205, 250),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold))
+        ],
       ),
     );
   }
